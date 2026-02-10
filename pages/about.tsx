924f869c403f53a6f5acff73a538b156dd5017bf
Ubuntu <ubuntu@ip-172-31-24-20.ec2.internal>
2026-02-10 16:59:53 +0000

    refactored repo for nextjs

---
+++ pages/about.tsx
@@ -0,0 +1,11 @@
+export default function About() {
+  return (
+    <main style={{ padding: '2rem' }}>
+      <h1>About Us</h1>
+      <p>
+        Welcome to our site! This is a simple About page created with Next.js.
+      </p>
+    </main>
+  );
+}
+
---
+++ pages/api/hello.js
@@ -0,0 +1,5 @@
+// hello.js
+export default function handler(req, res) {
+  res.status(200).json({message: 'Hello from the backend!'});
+}
+
