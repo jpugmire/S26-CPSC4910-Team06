b0c6fbc34c234100c8fde4fb218ce1ba3d2ce9c5
Price Medlin <pricemedlin3234@gmail.com>
2026-02-16 09:19:33 -0500

    Implemented account page (sorry forgot to work on a branch)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css"
+import Providers from "./providers"
 
 export const metadata = {
   title: 'Next.js',
@@ -12,7 +13,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body><Providers>{children}</Providers></body>
     </html>
   )
 }
