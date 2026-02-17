2cdcd419edcdc043050484ed0332afeb1366c101
Joseph Pugmire <jpugmir@clemson.edu>
2026-02-17 13:41:22 -0500

    remove Navbar from about page, since it can be accessed without being logged in.

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -9,7 +9,6 @@ export default async function AboutPage() {
 
   return (
     <>
-      <Navbar />
       <div className="flex min-h-screen items-center justify-center bg-gray-100">
         <div className="bg-white shadow-md rounded-lg p-8">
           <h1 className="text-2xl font-bold mb-4">About Page</h1>
