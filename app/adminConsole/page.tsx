cc18820b24a39f73fde548ed3d1255e95a6bb18e
Joseph Pugmire <jpugmir@clemson.edu>
2026-02-17 11:35:50 -0500

    Add navbar to each page, build out navbar component to match dashboard, show username on navbar

--- app/adminConsole/page.tsx
+++ app/adminConsole/page.tsx
@@ -1,6 +1,7 @@
 import { auth } from "@/auth"
 import { redirect } from "next/navigation"
 import { CreateUserForm } from "@/components/userCreate-form"
+import Navbar from "@/components/navbar"
 
 export default async function AdminPage() {
   const session = await auth()
@@ -12,15 +13,18 @@ export default async function AdminPage() {
   if (session.user?.role !== "A") redirect("/dashboard")
 
   return (
-    <div className="flex min-h-screen items-center justify-center bg-gray-100">
-          <div className="w-full max-w-md">
-            <div className="bg-white shadow-md rounded-lg p-8">
-              <h1 className="text-2xl font-bold text-center mb-6">
-                Create New User
-              </h1>
-              <CreateUserForm />
-            </div>
+    <>
+      <Navbar />
+      <div className="flex min-h-screen items-center justify-center bg-gray-100">
+        <div className="w-full max-w-md">
+          <div className="bg-white shadow-md rounded-lg p-8">
+            <h1 className="text-2xl font-bold text-center mb-6">
+              Create New User
+            </h1>
+            <CreateUserForm />
           </div>
         </div>
+      </div>
+    </>
   )
 }
