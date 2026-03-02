cad0072fabca280a3123755e4d6adc94a40b2670
Jake Hatchell <jwhatch@clemson.edu>
2026-03-02 16:15:30 -0500

    Added necessary files and sponsor console for sponsor users.

---
+++ app/sponsorConsole/page.tsx
@@ -0,0 +1,21 @@
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
+import Link from "next/link";
+// import SponsorPanel from "@/components/sponsor-panel";
+import Navbar from "@/components/navbar";
+
+export default async function SponsorPage() {
+  const session = await auth();
+
+  // not logged in
+  if (!session) redirect("/login");
+
+  // not admin
+  if (session.user?.role !== "S") redirect("/dashboard");
+
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+}
