757a115773f23065a148eec64f3b02e95eebaec9
Technicalitics <9w8juyib@exemplify4236.anonaddy.com>
2026-03-09 14:51:17 -0400

    added navbar to about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -9,6 +9,7 @@ export default async function AboutPage() {
 
   return (
     <>
+      <Navbar />
       <div className="flex min-h-screen items-center justify-center bg-gray-100">
         <div className="bg-white shadow-md rounded-lg p-8">
           <h1 className="text-2xl font-bold mb-4">About Page</h1>
