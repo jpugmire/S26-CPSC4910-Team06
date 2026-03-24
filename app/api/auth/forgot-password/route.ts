41c7f06cb45951aacd60733d3edb4db464afe0cc
Joseph Pugmire <jpugmir@clemson.edu>
2026-03-24 14:49:42 -0400

    adjust forgot-password route.ts

--- app/api/auth/forgot-password/route.ts
+++ app/api/auth/forgot-password/route.ts
@@ -3,9 +3,9 @@ import crypto from "crypto"
 import { Resend } from "resend"
 import { prisma } from "@/lib/prisma"
 
-const resend = new Resend(process.env.RESEND_API_KEY)
 
 export async function POST(req: NextRequest) {
+    const resend = new Resend(process.env.RESEND_API_KEY)
     const { email } = await req.json()
 
     if (!email) {
